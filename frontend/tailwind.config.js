a54da98c98b2f13cdf6c1dfc090806f9219e3e7f
kairo913 <148944926+kairo913@users.noreply.github.com>
2024-03-07 15:46:34 +0900

    style: Add formatter plugins

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
     content: ["./src/**/*.{html,js,svelte,ts}"],
     theme: {
-        extend: {},
+        extend: {
+            keyframes: {
+                zoom: {
+                    "0%": "transform: scale(0.95)",
+                    "100%": "transform: scale(1)",
+                },
+                fade: {
+                    "0%": "opacity: 0",
+                    "100%": "opacity: 1",
+                },
+            },
+            animation: {
+                zoom: "zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1)",
+                fade: "fade 0.2s ease-out",
+            },
+        },
     },
     plugins: [],
 };
