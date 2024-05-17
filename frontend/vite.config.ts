f368fc14fc49c541a8078f61409398570de99d66
kairo913 <148944926+kairo913@users.noreply.github.com>
2024-05-17 09:39:03 +0900

    chore: Refactored frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,10 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import VitePluginBrowserSync from "vite-plugin-browser-sync";
 
 export default defineConfig({
-    plugins: [sveltekit(), VitePluginBrowserSync()],
+    plugins: [sveltekit()],
     server: {
-        watch: {
-            usePolling: true,
-            interval: 1000,
-        },
+        host: true,
+        port: 3000,
     },
 });
