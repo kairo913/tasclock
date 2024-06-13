bcc505134af365b2c870b0fa6a644be44ff88ff1
kairo913 <148944926+kairo913@users.noreply.github.com>
2024-06-13 09:40:12 +0900

    chore: Add unique constraint to email column in users table

--- db/init/init.sql
+++ db/init/init.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS tasclock.users (
     `id` SERIAL PRIMARY KEY,
     `firstname` VARCHAR(255) NOT NULL,
     `lastname` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
     `password` VARCHAR(255) NOT NULL,
     `salt` VARCHAR(255) NOT NULL,
     `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
